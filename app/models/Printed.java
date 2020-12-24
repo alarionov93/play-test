6c1e03967339797181a2565cffa6f3b6c9f7dc4b
alarionov93 <alarionov93@yandex.ru>
2020-12-24 18:19:57 +0500

    ebean with ebean

---
+++ app/models/Printed.java
@@ -0,0 +1,47 @@
+package models;
+
+//import org.joda.time.DateTime;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import play.data.format.Formats;
+import io.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by sanya on 20.01.16.
+ */
+@MappedSuperclass //this annotation is used for telling ebean that models have a parent class
+public abstract class Printed extends Model {
+
+    public String title;
+
+    @Formats.DateTime(pattern="yyyy-MM-dd")
+    public DateTime jDate = new DateTime(); //TODO: do something with this - forms are getting invalid date error
+
+    public Date date = jDate.toDate();
+    //TODO: make a constructor with input date assignment!!!
+//    protected DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+//    public DateTime date = formatter.parseDateTime(new DateTime().toString().split("T")[0]);
+
+    public String date__str() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+        String date = jDate.toString().split("T")[0];
+        DateTime dt = formatter.parseDateTime(date);
+        return dt.toString().split("T")[0];
+    }
+
+    public int numberOfPages;
+
+    // public static Finder<Long, Printed> find;
+
+    // static {
+    //     find = new Finder<>(Long.class, Printed.class);
+    // }
+}
---
+++ app/models/Task.java
@@ -0,0 +1,43 @@
+package models;
+
+import javax.persistence.*;
+import play.data.validation.Constraints.*;
+import java.util.*;
+
+/**
+ * Created by sanya on 17.01.16.
+ */
+
+@Entity
+public class Task extends Printed {
+
+    @Id
+    public Long id; //fields can not be even protected !!
+
+    public String label;
+
+    // public static Finder<Long, Task> find;
+
+    // static {
+    //     find = new Finder<>(
+    //             Long.class, Task.class
+    //     );
+    // }
+
+    // public static List<Task> all() {
+    //     return find.all();
+    // }
+
+    // public static Task get(Long id) {
+    //     return find.byId(id);
+    // }
+
+    public static void create(Task task) {
+        task.save();
+    }
+
+    // public static void delete(Long id) {
+    //     find.ref(id).delete();
+    // }
+
+}
